6feeb443181ef76f455652cc1f07abd40746ca35
Matthew <Spudermanigo@gmail.com>
2022-01-19 11:15:45 +0500

    Changed master branch

--- proj/proj/Program.cs
+++ proj/proj/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine($"First line");
+﻿    Console.WriteLine("Введите число A");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    int b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Выберите какую операцию Вы хотите выполнить с числами\n1 - Сложить\n2 - Вычесть\n3 - Умножить\n4 - Поделить");
+    int choose = int.Parse(Console.ReadLine());
+    switch (choose)
+    {
+        case 1:
+            {
+                addnumbers();
+            }
+            break;
+        case 2:
+            {
+                subtractnumbers();
+            }
+            break;
+        case 3:
+            {
+                multilpynumbers();
+            }
+            break;
+        case 4:
+            {
+                dividenumbers();
+            }
+            break;
+
+        default:
+            break;
+    }
